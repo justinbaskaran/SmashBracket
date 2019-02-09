5e0d9e4abe4bdbd1594fb2d0642e7a1b8cbe722d
justinbaskaran <justinbaskaran5@gmail.com>
2019-02-09 08:48:07 -0500

    update

--- SmashBracket.py
+++ SmashBracket.py
@@ -93,8 +93,54 @@ def bracket_setup():
             my_seed = int(input("What seed is " + str(player) + "?\n"))
             player.seed = my_seed
     player_list = sort_players(player_list)
-    bracket = Bracket(bracket_size, player_list)
+  
+    print(len(player_list))
+    #playersNum = checkPlayers(17,17)
+    playersNum = checkPlayers(len(player_list),len(player_list))
+    print (playersNum)
 
+    if playersNum - len(player_list) > 0:
+        for x in range(playersNum - len(player_list)):
+            name = "Person 0: Free Win"
+            character = "p0"
+            player = Player(name, character)
+            player_list.append(player) 
+        
+
+    bracket = Bracket(playersNum, player_list)
+
+    print("--------Tournament Bracket------------")
+    for y in range (0,playersNum//2):
+        print("--------Bracket:------------")
+        print ("Player 1:" + player_list[y].name)
+        print(y)
+        if y+1 != playersNum:
+            y = y+1
+        print ("Player 2: " + player_list[y].name)
+        print("-----------------------------")
+        if  y+1 != playersNum:
+            y = y+1
+
+
+
+
+def checkPlayers(players,playersDecrement):
+    print("Fixed Length: " + str(players))
+    print ("Players Decrement: " + str(playersDecrement) )
+    if playersDecrement == 1.0:
+        return players
+
+    if playersDecrement % 2 == 0:
+        return checkPlayers(players,playersDecrement/2)
+    elif playersDecrement %2 == 1 :
+        players = players + 1
+        print("Fixed Length After: " + str(players))
+        if players % 2 == 0:
+            return checkPlayers(players,players)
+        if players % 2 == 1:
+            return checkPlayers(players+1,players+1)
+        
+        
 
 
 
